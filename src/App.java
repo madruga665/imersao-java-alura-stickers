eef058f9f4829e01ad6071c6b36ee153c1fe0052
Luciano Amâncio <madruga665@hotmail.com>
2023-03-28 02:40:41 -0300

    add metodo showRating

--- src/App.java
+++ src/App.java
@@ -7,26 +7,31 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
-	public static void main(String[] args) throws Exception {
-		String topMoviesUrl = "https://raw.githubusercontent.com/lukadev08/lukadev08.github.io/main/apidata/imdbtop250moviesdata.json";
-		URI url = URI.create(topMoviesUrl);
-		HttpClient clientHttp = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder(url).GET().build();
-		HttpResponse<String> response = clientHttp.send(request, BodyHandlers.ofString());
-		String body = response.body();
-		JsonParser parser = new JsonParser();
-		List<Map<String, String>> moviesList = parser.parse(body);
 
-		for (Map<String, String> movie : moviesList) {
-			System.out.println(movie.get("title"));
-			System.out.println(movie.get("image"));
-			System.out.println(movie.get("imDbRating"));
-			Double imDbRating = Double.parseDouble(movie.get("imDbRating"));
+  static void showRating(Double rating) {
+    for (int i = 0; i < rating; i++) {
+      System.out.print("* ");
+    }
+  }
 
-			for (int i = 0; i < imDbRating; i++) {
-				System.out.print("* ");
-			}
-			System.out.println();
-		}
-	}
+  public static void main(String[] args) throws Exception {
+    String topMoviesUrl = "https://raw.githubusercontent.com/lukadev08/lukadev08.github.io/main/apidata/imdbtop250moviesdata.json";
+    URI url = URI.create(topMoviesUrl);
+    HttpClient clientHttp = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder(url).GET().build();
+    HttpResponse<String> response = clientHttp.send(request, BodyHandlers.ofString());
+    String body = response.body();
+    JsonParser parser = new JsonParser();
+    List<Map<String, String>> moviesList = parser.parse(body);
+
+    for (Map<String, String> movie : moviesList) {
+      System.out.println(movie.get("title"));
+      System.out.println(movie.get("image"));
+      System.out.println(movie.get("imDbRating"));
+      Double imDbRating = Double.parseDouble(movie.get("imDbRating"));
+      showRating(imDbRating);
+
+      System.out.println();
+    }
+  }
 }
